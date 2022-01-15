ec1ff351d2c3518882d5652167377d49578dcb4d
ILHWANK <50158086+ILHWANK@users.noreply.github.com>
2022-01-15 21:26:20 +0900

    FlyweightPettarn
    FlyweightPettarn

---
+++ Assets/Script/Flyweight/FieldObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldObject
+{
+    public bool isAttackEnable = false;
+    public string objectName = "";
+
+    public FieldObject(bool _isAttackEnable, string _objectName) {
+        isAttackEnable = _isAttackEnable;
+        objectName = _objectName;
+    }
+}
